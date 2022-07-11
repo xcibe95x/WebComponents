1901acf2348e0b0dd577161aea74d26b31756934
Mauro Leoci <xcibe95x@gmail.com>
2022-07-11 19:23:51 +0200

    Add Toggle

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# WebComponents
-Learning Web Components, i'm gonna update this open source repo, while i learn WebComponents my self.
+# Web Components
+
+## Why i made this repo?
+I need to quickly learn about Web Components, so i decided to share my researches and examples about it.
+
+## What are Web Components?
+Set of web platform API's that allow us to create custom reusable and encapsulated html tags to use in web pages and web apps
+Web components do not require any special 3rd party libraries or frameworks (React, Angular, etc) but can certainly be used with them.
+
+Web Components are made of 3 main building blocks:
+* Custom Elements
+* Shadow DOM
+* HTML Templates
+
+Let's take a look at them:
+
+## Custom Elements
+* Create Custom HTML tags
+* Create custom class
+* Lifecycle methods available
+
+As an example this is how we will write it
+``` js
+class AppDrawer extends HTMLElement {...}
+window.customElements.define('app-drawer', AppDrawer);
+```
+And this is how we will call it in HTML
+(Pretty similiar to React when we make Components and pass them props.)
+``` html
+<app-drawer></app-drawer>
+```
+
+## Custom Elements <small>LifeCycle Methods</small>
+* <b>constructor():</b> Called when an instance of the element is created or upgraded.
+* <b>connectedCallback(): Called every time when the elment is inserted into the DOM</b>
+* <b>attributeChangedCallback(attributeName, oldValue, newValue):</b> Called when an attribute is added, removed, updated, or replaced.
+
+## Shadow DOM
+* Used for self-contained components
+* Encapsulate styles and markup
+* Create with element.attachShadow({ mode: open })
+
+* Creates a "ShadowRoot" that we can reference and interact with
+
+## HTML Templates
+* Define the encapsulated markup of our web component
+* Template tag stores markup on page
+* Include both HTML and CSS in templates
+* Use slots to add custom text
+
+# Sources
+Traversy Media (Crash Course): https://www.youtube.com/watch?v=PCWaFLy3VUo
+
---
+++ userCard.js
@@ -0,0 +1,79 @@
+// Create a new template element
+const template = document.createElement('template');
+
+// Make a template using backticks
+template.innerHTML = `
+<style>
+    .user-card {
+        background: grey;
+        border-radius: 8px;
+        display: flex;
+        justify-content: center;
+    }
+</style>
+
+<div class="user-card">
+<div class="user-avatar">
+<img />
+    <h3>Jhon Doe</h3>
+    <div class="info">
+        <h4><slot name="email"/></h4>
+        <h4><slot name="phone"/></h4>
+    </div>
+        <button id="toggle-info">Hide Info</button>
+    </div>
+</div>
+`; 
+
+// We make a Class and extend it as an HTMLElement
+class UserCard extends HTMLElement {
+    // We run a constructor
+    constructor() {
+        // We use super to call the parent constructor
+        super();
+
+        this.showInfo = true;
+
+        // Create a Shadow DOM and Attach options with mode: open
+        this.attachShadow({ mode: 'open' });
+
+        // Append our template to the ShdowRoot
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        // Target Attribues to add custom attributes
+        this.shadowRoot.querySelector ('h3').innerText = this.getAttribute('name');
+        this.shadowRoot.querySelector ('img').src = this.getAttribute('avatar');
+        
+        // We reference our component with the this keyword and target it's innerHTML
+        // Attribute example without Shadow DOM: this.innerHTML = `${this.getAttribute('name')}`;
+    }
+
+    toggleInfo() {
+        this.showInfo = !this.showInfo;
+
+        const info = this.shadowRoot.querySelector('.info');
+        const toggleBtn = this.shadowRoot.querySelector('#toggle-info');
+
+        if (this.showInfo) {
+            info.style.display = 'block';
+            toggleBtn.innerText = 'Hide Info';
+        } else {
+            info.style.display = 'none';
+            toggleBtn.innerText = 'Show Info';
+        }
+    }
+
+    connectedCallback() {
+        this.shadowRoot.querySelector('#toggle-info')
+        .addEventListener('click', () => this.toggleInfo());
+    }
+
+    disconnetedCallback() {
+        this.shadowRoot.querySelector('#toggle-info')
+        .removeEventListener('click', () => this.toggleInfo());
+    }
+
+}
+
+// We define our component html tag and bind it to the class
+window.customElements.define('user-card', UserCard);
